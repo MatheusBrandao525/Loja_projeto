56657e690e6d009d9c5238bb7d34ac4fc5bcf628
Matheus Brandão <mafe123silva@gmail.com>
2021-06-27 13:50:32 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Minha Loja</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrap.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <nav>
+        <div class="conteiner">
+
+            <div class="logo">
+                <h1>Logo-tipo</h1>
+            </div><!--logo-->
+
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Pagina01</a></li>
+                    <li><a href="#">Pagina02</a></li>
+                    <li><a href="#">Pagina03</a></li>
+                    <li><a href="#">Contato</a></li>
+                    <li><a href="#">Sobre</a></li>
+                </ul>
+                <div class="clear"></div><!--clear-->
+            </div><!--menu-->
+
+            <div class="barra-pesquisar">
+                    <input type="text" id="produto" placeholder="Buscar produtos...">
+                    <button id="buscar">Buscar</button>
+            </div><!--barra-pesquisar-->
+        </div><!--conteiner-->
+    </nav>
+
+    
+
+</body>
+</html>
